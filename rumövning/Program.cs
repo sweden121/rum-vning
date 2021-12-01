4a45cae1cf0e8f63e2d485d9915382096906bcd0
adriano mesquita <sweden1244@gmail.com>
2021-12-01 12:42:12 +0100

    blyat

---
+++ rumövning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rumövning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
