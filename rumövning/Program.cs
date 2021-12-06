a34b9ea0dad6b08ca4c09b54537741138931651e
adriano mesquita <sweden1244@gmail.com>
2021-12-06 14:53:02 +0100

    den nya rummet

--- rumövning/Program.cs
+++ rumövning/Program.cs
@@ -1,15 +1,73 @@
 ﻿using System;
 
-namespace rumövning
+string room = "mainroad";
+
+while (room != "end")
 {
-    class Program
+    if (room == "mainroad")
     {
-        static void Main(string[] args)
+        Console.WriteLine("You are now on the mainroad. In front of you is a small sideroad, leading to the Low Road.");
+
+        Console.WriteLine("Where do you want to go?");
+        string where = Console.ReadLine();
+
+        if (where == "down")
         {
-            Console.WriteLine("Hello World!");
+            room = "lowroad";
+        }
+        if (where == "right")
+        {
+            room = "forest"; // den nya rummet
+        }
+        else
+        {
+            Console.WriteLine("I'm sorry, what?");
+        }
+    }
+    else if (room == "lowroad")
+    {
+        Console.WriteLine("You are now on the Low Road. In front of you is a small stairway, leading up to the mainroad.");
+
+        Console.WriteLine("Where do you want to go?");
+        string where = Console.ReadLine();
+
+        if (where == "up")
+        {
+            room = "mainroad";
+        }
+        else if (where == "out")
+        {
+            room = "end";
+        }
+        else
+        {
+            Console.WriteLine("I'm sorry, what?");
+        }
+    }
+    else if (room == "forest") // den nya rummet
+    {
+
+        Console.WriteLine("you are in the forest you can go to the side road and the mainroad from here where you are ");
+
+        Console.WriteLine("where do you want to go down for the lowrode and left for the main road");
+        string where = Console.ReadLine();
+
+        if (where == "down") // vägen till lowroad
+        {
+            room = "lowroad";
+        }
+        if (where == "left")
+        {
+            room = "mainroad";
+        }
+        else
+        {
+            Console.WriteLine("im sorry what?"); 
         }
     }
 }
+
+
 
 
 
